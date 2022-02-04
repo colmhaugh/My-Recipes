468518476bc6728028a3cc7d519cf32ee9afb4c3
colmhaugh <colm.haugh@gmail.com>
2022-02-04 18:12:03 +0000

    Add dirs

--- recipes/models.py
+++ recipes/models.py
@@ -3,26 +3,19 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
-class Course(models.Model):
-    title = models.CharField(max_length=200) 
-    slug = models.SlugField(max_length=200, unique=True)
-
-    def __str__(self):
-        return self.title
-
-
 class Recipe(models.Model):
     title = models.CharField(max_length=200, unique=True) 
     slug = models.SlugField(max_length=200, unique=True)
-    image = CloudinaryField('image', default='placeholder')
-    description = models.TextField()    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
+    created_on = models.DateTimeField(auto_now_add=True)    
     ingredients = models.TextField()
-    directions = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(
-        User, related_name='blogpost_like', blank=True)
-    course = models.ForeignKey(
-        Course, on_delete=models.CASCADE)
+    instructions = models.TextField()
+    excerpt = models.TextField(blank=True)
+    image = CloudinaryField('image', default='placeholder')
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+    # course = models.ForeignKey(
+    #    Course, on_delete=models.CASCADE)    
+    # description = models.TextField()    
 
     class Meta:
         ordering = ["-created_on"] #Might change to number of likes
@@ -34,17 +27,25 @@ class Recipe(models.Model):
         return self.likes.count()
 
 
-class Comment(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
-                             related_name="comments")
-    user_name = models.ForeignKey(
-        User, on_delete=models.CASCADE)    
-    body = models.TextField()
+class Comment(models.Model):    
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="comments")
+    comment_text = models.TextField(max_length=500)
+    user_name = models.CharField(max_length=50)                       
+    # user_name = models.ForeignKey(
+    #     User, on_delete=models.CASCADE)    
     created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.comment_text} by {self.user_name}"
+
+#COURSE = ((0, "Starter"), (1, "Main"), (2, "Dessert"))
+
+# class Course(models.Model):
+#     title = models.CharField(max_length=200) 
+#     slug = models.SlugField(max_length=200, unique=True)
+
+#     def __str__(self):
+#         return self.title
